c4264409530f705a398c68b3952252af94995937
Carlos Parada <carlosparadaandrade@gmail.com>
2022-01-26 23:59:35 +0100

    Beginning of the project structure and Icon

---
+++ Marvel/Common/Helpers/Configuration.swift
@@ -0,0 +1,56 @@
+//
+//  Configuration.swift
+//  Marvel
+//
+//  Created by Carlos Parada on 26/01/22.
+//
+
+import Foundation
+
+enum ConfigurationKey: String {
+    case baseUrl = "BASE_URL"
+    case publicKeySign = "PUBLIC_KEY_SIGN"
+}
+
+class Configuration {
+    static let shared = Configuration()
+    
+    func getValueConfiguration<T> (withType _:T.Type, key:ConfigurationKey) -> T? {
+        
+        if let configurations = getConfigurations() {
+            return configurations.object(forKey: key.rawValue) as? T
+        }
+        return nil
+    }
+    
+    private func getConfigurations () -> NSDictionary? {
+        var bundle = Bundle.main
+        if bundle.bundleURL.pathExtension == "appex" {
+            bundle = (Bundle(url: bundle.bundleURL.deletingLastPathComponent())) ?? bundle
+        }
+        let envsPListPath = bundle.path(forResource: MarvelConstants.Configuration.configurations, ofType: ".plist")
+        guard let passEncryptedData = try? NSData(contentsOfFile: envsPListPath ?? "") as Data else {
+            return nil
+        }
+        do {
+          
+            if let enviroments = try PropertyListSerialization.propertyList(from: passEncryptedData,
+                                                                            options: .mutableContainersAndLeaves,
+                                                                            format: nil) as? [String:Any] {
+                let obj = enviroments[getEnvironment()]
+                return obj as? NSDictionary
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return nil
+    }
+    
+     private func getEnvironment() -> String {
+        let dictionary: NSDictionary = Bundle.main.infoDictionary! as NSDictionary
+        let env: String? = dictionary.object(forKey: MarvelConstants.Configuration.configuration) as? String
+        return env ?? ""
+    }
+    
+}
---
+++ Marvel/Common/Utils/MarvelConstants.swift
@@ -0,0 +1,22 @@
+//
+//  MarvelConstants.swift
+//  Marvel
+//
+//  Created by Carlos Parada on 26/01/22.
+//
+
+import Foundation
+
+struct MarvelConstants {
+    struct General {
+    }
+    
+    struct Configuration {
+        static let configurations = "Configurations"
+        static let configuration = "Configuration"
+    }
+    struct Splash {
+        /// minimum time to keep the splash open
+        static let minimumTime:Double = 2.5
+    }
+}
